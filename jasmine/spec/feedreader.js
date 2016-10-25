105277bb5e907ff6911dd534fff10e7852f131b9
jcast90 <jcast90@me.com>
2016-10-25 10:39:52 -0700

    beautify

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,17 +3,16 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
-$(function () {
+$(function() {
     /* This is our first test suite - a test suite just contains
      * a related set of tests. This suite is all about the RSS
      * feeds definitions, the allFeeds variable in our application.
      */
-    describe('RSS Feeds', function () {
+    describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,7 +20,7 @@ $(function () {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function () {
+        it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -32,9 +31,9 @@ $(function () {
          * and that the URL is not empty.
          */
 
-        it('Url is defined', function () {
+        it('Url is defined', function() {
             /*loop through allfeeds URL making sure its defined and not empty*/
-            allFeeds.forEach(function (feed) {
+            allFeeds.forEach(function(feed) {
                 var feedUrl = feed.url;
                 expect(feedUrl).toBeDefined();
                 expect(feedUrl.length).not.toBe(0);
@@ -47,9 +46,9 @@ $(function () {
          * and that the name is not empty.
          */
 
-        it('Name is defined', function () {
+        it('Name is defined', function() {
             /*loops throught allfeeds name making sure they are defined and not empty*/
-            allFeeds.forEach(function (feed) {
+            allFeeds.forEach(function(feed) {
                 feedName = feed.name;
                 expect(feedName).toBeDefined();
                 expect(feedName.length).not.toBe(0);
@@ -60,7 +59,7 @@ $(function () {
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe('The menu', function () {
+    describe('The menu', function() {
 
 
         /* TODO: Write a test that ensures the menu element is
@@ -70,7 +69,7 @@ $(function () {
          */
 
         /*making sure the body has the class of menuhidden to make it hidden by default*/
-        it('is hidden', function () {
+        it('is hidden', function() {
             expect($(document.body).hasClass('menu-hidden')).toBe(true);
         });
 
@@ -79,7 +78,7 @@ $(function () {
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
-        it('toggles visibility on icon click', function () {
+        it('toggles visibility on icon click', function() {
             /*if else statement for if the menu-icon is clicked, change menu visibility*/
             $('a.menu-icon-link').click();
             expect(document.body.className).not.toContain('menu-hidden');
@@ -89,9 +88,9 @@ $(function () {
 
     });
     /* TODO: Write a new test suite named "Initial Entries" */
-    describe('Initial Entries', function () {
+    describe('Initial Entries', function() {
 
-        beforeEach(function (done) {
+        beforeEach(function(done) {
             loadFeed(0, done);
 
         });
@@ -101,19 +100,19 @@ $(function () {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        it('has an entry in feed container', function (done) {
+        it('has an entry in feed container', function(done) {
             /*making sure my .entry h2 has content in it*/
             expect($('.entry h2').length).not.toBe([0]);
             done();
         });
     });
     /* TODO: Write a new test suite named "New Feed Selection"*/
-    describe('New Feed Selection', function () {
+    describe('New Feed Selection', function() {
         /*watching if the 2nd feed is loaded*/
         var feed = $('.header-title').html();
-        beforeEach(function (done) {
-            loadFeed(0, function () {
-                loadFeed(1, function () {
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                loadFeed(1, function() {
                     done();
                 });
             });
@@ -123,7 +122,7 @@ $(function () {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        it('content changes', function (done) {
+        it('content changes', function(done) {
 
 
             expect(feed[0]).not.toBe(feed[1]);
